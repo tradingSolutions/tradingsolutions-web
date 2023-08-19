aa4d7d1191dbcea78aba326c9f6959a8c0405286
tradingSolutions <trading.solutions.mg23@gmail.com>
2023-08-19 11:31:56 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const slider = document.getElementById('slider');
+const container = slider.parentElement;
+let isMouseDown = false;
+
+function moveSlider(e) {
+    if (!isMouseDown) return;
+    let rect = container.getBoundingClientRect(),
+        x = e.clientX - rect.left;
+    x = Math.min(Math.max(0, x), rect.width); // Limitar entre 0 y el ancho del contenedor
+    slider.style.width = x + 'px';
+}
+
+document.addEventListener('mousedown', function(e) {
+    if (e.target === slider || e.target.parentNode === slider) {
+        isMouseDown = true;
+    }
+});
+
+document.addEventListener('mouseup', function() {
+    isMouseDown = false;
+});
+
+document.addEventListener('mousemove', moveSlider);
+
