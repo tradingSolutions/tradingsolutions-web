cfe186be58c8f4e43807650457db1c21038cd366
tradingSolutions <trading.solutions.mg23@gmail.com>
2023-08-26 12:10:16 -0300

    Error Fix - Deploy

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-const slider = document.getElementById('slider');
-const container = slider.parentElement;
-let isMouseDown = false;
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.getElementById('slider-id');
+    const container = document.getElementById('container-slider');
+    let isMouseDown = false;
 
-function moveSlider(e) {
-    if (!isMouseDown) return;
-    let rect = container.getBoundingClientRect(),
-        x = e.clientX - rect.left;
-    x = Math.min(Math.max(0, x), rect.width); // Limitar entre 0 y el ancho del contenedor
-    slider.style.width = x + 'px';
-}
-
-document.addEventListener('mousedown', function(e) {
-    if (e.target === slider || e.target.parentNode === slider) {
-        isMouseDown = true;
+    function moveSlider(e) {
+        if (!isMouseDown) return;
+        let rect = container.getBoundingClientRect(),
+            x = e.clientX - rect.left;
+        x = Math.min(Math.max(0, x), rect.width);
+        slider.style.width = x + 'px';
     }
-});
 
-document.addEventListener('mouseup', function() {
-    isMouseDown = false;
-});
+    document.addEventListener('mousedown', function(e) {
+        if (e.target === slider || e.target.parentNode === slider) {
+            isMouseDown = true;
+        }
+    });
 
-document.addEventListener('mousemove', moveSlider);
+    document.addEventListener('mouseup', function() {
+        isMouseDown = false;
+    });
 
-document.querySelectorAll('.navbar-nav a').forEach(link => {
-    link.addEventListener('click', () => {
-        // Cerrar el menú colapsado después de hacer clic en un enlace
-        document.querySelector('.navbar-toggler').click(); 
+    document.addEventListener('mousemove', moveSlider);
+
+        // Obtén una referencia al botón del menú desplegable (toggler)
+    const navbarToggler = document.querySelector('.navbar-toggler');
+
+    // Obtén una referencia a todos los enlaces de navegación
+    const navLinks = document.querySelectorAll('.navbar-nav a');
+
+    // Agrega un evento de clic a cada enlace
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            // Cierra el menú desplegable
+            navbarToggler.click();
+        });
     });
 });
